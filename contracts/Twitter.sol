5ef180dcceefbe5cfc8a23b6560a1dfcb9efcbd2
patil-prathamesh <patilprathamesh696@gmail.com>
2024-11-22 10:19:20 +0530

    added struct to the twitter contract

--- contracts/Twitter.sol
+++ contracts/Twitter.sol
@@ -3,17 +3,30 @@
 pragma solidity ^0.8.26;
 
 contract Twitter {
-    mapping(address => string[]) public tweets;
-
-    function createTweet(string memory _tweet) public {
-        tweets[msg.sender].push(_tweet);
+    struct Tweet {
+        address author;
+        string content;
+        uint256 timestamp;
+        uint256 likes;
     }
 
-    function getTweet(address _owner, uint _i) public view returns (string memory) {
+    mapping(address => Tweet[]) public tweets;
+
+    function createTweet(string memory tweet) public {
+        Tweet memory newTweet = Tweet({
+            author: msg.sender,
+            content: tweet,
+            timestamp: block.timestamp,
+            likes: 0
+        });
+        tweets[msg.sender].push(newTweet);
+    }
+
+    function getTweet(address _owner, uint256 _i) public view returns (Tweet memory) {
         return tweets[_owner][_i];
     }
 
-    function getAllTweets(address _owner) public view returns (string[] memory) {
+    function getAllTweets(address _owner)public view returns (Tweet[] memory) {
         return tweets[_owner];
     }
-} 
+}
